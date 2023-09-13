d36eea19c19e9498f35010641360d4dcde4f5270
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2023-09-13 16:07:16 +0300

    Update sudoku_generation.h
    The sudoku game can have a one or multiple valid solutions and the way of randomization only to generate a valid sudoku may generate not valid one. So after generating a random sudoku I check solving it using backtracking if it solved that means it has at least one solution.

--- include/sudoku_generation.h
+++ include/sudoku_generation.h
@@ -1,36 +1,33 @@
 #include "sudoku_validation.h"
 
-void generate9()
-{
-    int sudoku9[n][n];
-    int cnt = 81 - 35;
-    memset(sudoku9, 0, sizeof(sudoku9));
-    srand(time(0));
-    for(int i = 0; i < cnt; i++){
-        int r1 = rand() % 9, r2 = rand() % 9;
-        int x, y;
+bool rec6(int r, int c, int sudoku6[][m]){
+    if(r == n) return 1;
+    if(c == m) return rec6(r+1, 0, sudoku6);
+    if(sudoku6[r][c] != '.') return rec6(r, c+1, sudoku6);
 
-        while(sudoku9[r1][r2]){
-            r1 = rand() % 9;
-            r2 = rand() % 9;
+    for(int ch = 1; ch <= 6; ch++){
+        sudoku6[r][c] = ch;
+        if(valid6(r, c, sudoku6)){
+            if(rec6(r, c+1, sudoku6)) return 1;
         }
-
-        x = r1, y = r2;
-        for(int j = 1; j <= 9; j++){
-            sudoku9[x][y] = j;
-            if(valid9(x, y, sudoku9)){
-                break;
-            }
-            sudoku9[x][y] = 0;
-        }
+        sudoku6[r][c] = '.';
     }
+    return 0;
+}
 
-    for(int i = 0 ;i < n; i++){
-        for(int j = 0; j < n; j++){
-            printf("%d ", sudoku9[i][j]);
+bool rec9(int r, int c, int sudoku9[][n]){
+    if(r == n) return 1;
+    if(c == m) return rec9(r+1, 0, sudoku9);
+    if(sudoku9[r][c] != '.') return rec9(r, c+1, sudoku9);
+
+    for(int ch = 1; ch <= 9; ch++){
+        sudoku9[r][c] = ch;
+        if(valid9(r, c, sudoku9)){
+            if(rec9(r, c+1, sudoku9)) return 1;
         }
-        printf("%c", '\n');
+        sudoku9[r][c] = '.';
     }
+    return 0;
 }
 
 void generate6(){
@@ -57,9 +54,50 @@ void generate6(){
         }
     }
 
+    if(!rec6(0,0, sudoku6)){
+        generate6();
+    }
+
     for(int i = 0 ;i < m; i++){
         for(int j = 0; j < m; j++){
             printf("%d ", sudoku6[i][j]);
+        }
+        printf("%c", '\n');
+    }
+}
+
+void generate9()
+{
+    int sudoku9[n][n];
+    int cnt = 81 - 35;
+    memset(sudoku9, 0, sizeof(sudoku9));
+    srand(time(0));
+    for(int i = 0; i < cnt; i++){
+        int r1 = rand() % 9, r2 = rand() % 9;
+        int x, y;
+
+        while(sudoku9[r1][r2]){
+            r1 = rand() % 9;
+            r2 = rand() % 9;
+        }
+
+        x = r1, y = r2;
+        for(int j = 1; j <= 9; j++){
+            sudoku9[x][y] = j;
+            if(valid9(x, y, sudoku9)){
+                break;
+            }
+            sudoku9[x][y] = 0;
+        }
+    }
+
+    if(!rec9(0,0, sudoku9)){
+        generate9();
+    }
+
+    for(int i = 0 ;i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", sudoku9[i][j]);
         }
         printf("%c", '\n');
     }
