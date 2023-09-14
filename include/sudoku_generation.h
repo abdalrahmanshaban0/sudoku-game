9f3932530886863a707ada018f23b5a8fa165960
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2023-09-14 17:40:04 +0300

    Update sudoku_generation.h

--- include/sudoku_generation.h
+++ include/sudoku_generation.h
@@ -1,38 +1,38 @@
 #include "sudoku_validation.h"
 
 bool rec6(int r, int c, int sudoku6[][m]){
-    if(r == n) return 1;
-    if(c == m) return rec6(r+1, 0, sudoku6);
-    if(sudoku6[r][c] != '.') return rec6(r, c+1, sudoku6);
+    if(r == 6) return 1;
+    if(c == 6) return rec6(r+1, 0, sudoku6);
+    if(sudoku6[r][c]) return rec6(r, c+1, sudoku6);
 
     for(int ch = 1; ch <= 6; ch++){
         sudoku6[r][c] = ch;
         if(valid6(r, c, sudoku6)){
             if(rec6(r, c+1, sudoku6)) return 1;
         }
-        sudoku6[r][c] = '.';
+        sudoku6[r][c] = 0;
     }
     return 0;
 }
 
 bool rec9(int r, int c, int sudoku9[][n]){
-    if(r == n) return 1;
-    if(c == m) return rec9(r+1, 0, sudoku9);
-    if(sudoku9[r][c] != '.') return rec9(r, c+1, sudoku9);
+    if(r == 9) return 1;
+    if(c == 9) return rec9(r+1, 0, sudoku9);
+    if(sudoku9[r][c]) return rec9(r, c+1, sudoku9);
 
     for(int ch = 1; ch <= 9; ch++){
         sudoku9[r][c] = ch;
         if(valid9(r, c, sudoku9)){
             if(rec9(r, c+1, sudoku9)) return 1;
         }
-        sudoku9[r][c] = '.';
+        sudoku9[r][c] = 0;
     }
     return 0;
 }
 
 void generate6(){
     int sudoku6[m][m];
-    int cnt = 36 - 18;
+    int cnt = 36 - 22;
     memset(sudoku6, 0, sizeof(sudoku6));
     srand(time(0));
     for(int i = 0; i < cnt; i++){
@@ -54,7 +54,14 @@ void generate6(){
         }
     }
 
-    if(!rec6(0,0, sudoku6)){
+    int temp[6][6];
+    for(int i = 0 ;i < 6; i++){
+        for(int j = 0 ;j < 6; j++){
+            temp[i][j] = sudoku6[i][j];
+        }
+    }
+
+    if(!rec6(0,0, temp)){
         generate6();
     }
 
@@ -69,7 +76,7 @@ void generate6(){
 void generate9()
 {
     int sudoku9[n][n];
-    int cnt = 81 - 35;
+    int cnt = 81 - 55;
     memset(sudoku9, 0, sizeof(sudoku9));
     srand(time(0));
     for(int i = 0; i < cnt; i++){
@@ -91,7 +98,14 @@ void generate9()
         }
     }
 
-    if(!rec9(0,0, sudoku9)){
+   int temp[9][9];
+    for(int i = 0 ;i < 9; i++){
+        for(int j = 0 ;j < 9; j++){
+            temp[i][j] = sudoku9[i][j];
+        }
+    }
+
+    if(!rec9(0,0, temp)){
         generate9();
     }
 
